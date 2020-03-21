5cdf39185fc9ace76306e700ed74032dcdcb9297
Oksana Tukach <okstukach@gmail.com>
2020-03-21 12:22:43 +0300

    feat: add DOM

---
+++ script.js
@@ -0,0 +1,202 @@
+/* Header */
+document.querySelector('#nav').addEventListener('click', change_color);
+function change_color(event) {
+    document.querySelectorAll('.header__nav').forEach( element => {
+        element.classList.remove('active');
+    });
+
+    if (event.target.tagName === "A") {
+        event.target.parentElement.classList.add('active'); 
+    }
+}
+    const menuBlock = document.querySelectorAll('#header__navigation li');
+    const serviceBlock = document.getElementById('services');
+    const portfolioBlock = document.getElementById('portfolio');
+    const aboutBlock = document.getElementById('about');
+    const contactBlock = document.getElementById('contact');
+    const headerHeight = document.getElementById('header').offsetHeight;
+  
+    document.getElementById('header__navigation').addEventListener('click', () => {
+      menuBlock.forEach(element => element.classList.remove('active'));
+      event.target.classList.add('active');
+    });
+  
+    window.addEventListener('scroll', () => {
+      if (window.scrollY < serviceBlock.offsetTop - headerHeight) {
+        menuBlock.forEach(element => element.classList.remove('active'));
+        document.querySelector('.header__home').classList.add('active');
+      };
+  
+      if (window.scrollY >= serviceBlock.offsetTop - headerHeight
+        && window.scrollY < portfolioBlock.offsetTop - headerHeight) {
+        menuBlock.forEach(element => element.classList.remove('active'));
+        document.querySelector('.header__services').classList.add('active');
+      };
+  
+      if (window.scrollY >= portfolioBlock.offsetTop - headerHeight
+        && window.scrollY < aboutBlock.offsetTop - headerHeight) {
+        menuBlock.forEach(element => element.classList.remove('active'));
+        document.querySelector('.header__portfolio').classList.add('active');
+      };
+  
+      if (window.scrollY >= aboutBlock.offsetTop - headerHeight
+        && window.scrollY < contactBlock.offsetTop - headerHeight) {
+        menuBlock.forEach(element => element.classList.remove('active'));
+        document.querySelector('.header__about').classList.add('active');
+      };
+  
+      if (window.scrollY >= contactBlock.offsetTop - headerHeight
+        || document.documentElement.scrollTop + document.documentElement.clientHeight == document.documentElement.scrollHeight
+        || window.scrollY > document.documentElement.scrollHeight - document.documentElement.clientHeight) {
+        menuBlock.forEach(element => element.classList.remove('active'));
+        document.querySelector('.header__contact').classList.add('active');
+      };
+    });
+
+/* Portfolio */
+
+function change_color_text_portfolio(){
+    tags_portfolio.forEach(function(item){
+        item.style.color = "#666d89";
+        item.style.borderColor = "#666d89";
+    });
+    this.style.color = "#c5c5c5";
+    this.style.borderColor  = '#c5c5c5';
+
+    let temp = img_form_portfolio[0].cloneNode();
+    temp.onclick = change_border_image_portfolio;
+    parent_images.removeChild(img_form_portfolio[0]);
+    parent_images.insertBefore(temp, img_form_portfolio[12]);
+    img_form_portfolio = Array.from(document.getElementsByClassName('portfolio__image'));
+}
+
+function change_border_image_portfolio(){
+    img_form_portfolio.forEach(function(item){
+        item.style.borderWidth= "0px";
+        item.style.left = "0px";
+        item.style.top = "0px";
+    });
+
+    this.style.borderWidth = "5px";
+    this.style.left = "-5px";
+    this.style.top = "-5px";
+}
+
+let img_form_portfolio = Array.from(document.getElementsByClassName('portfolio__image'));
+let tags_portfolio = Array.from(document.getElementById("portfolio__button").getElementsByTagName('input'));
+let parent_images = document.getElementById("portfolio__images");
+
+tags_portfolio.forEach(element => element.onclick = change_color_text_portfolio);
+
+img_form_portfolio.forEach(element => element.onclick = change_border_image_portfolio);
+
+/* Get a Quote*/
+function sumbmit_form(){
+    let subject_message = subject.value == '' ? 'Без темы' : 'Тема: ' + subject.value;
+    let discribe_message = discribe.value == '' ? 'Без описания' : 'Описание: ' + discribe.value;
+
+    document.body.insertAdjacentHTML('beforeend',
+                                '<div id="message" style="position: fixed; top:0px; left:0px; width: 100vw; height: 100vh; background-color: rgba(0, 0, 0, 0.5); z-index: 11; display: flex; justify-content: center; align-items: center; "> ' +
+                                    '<div class="content" style="padding:40px; background-color: white; border-radius: 10px; width: 500px; height: 200px; display: flex; flex-direction: column; justify-content: space-around; align-items: center; color: black">' +   
+                                        '<h3>Письмо отправлено</h3>'+
+                                        '<h4>' + subject_message + '</h4>'+
+                                        '<p style="position: relative; top:0px;">' + discribe_message + '</p>'+
+                                        '<button onclick="message_button_click()"style="width: 100px">OK</button>'+
+                                    '</div>'+
+                                '</div>');
+    return false;
+}
+
+function message_button_click(){
+    name.value = '';
+    email.value = '';
+    subject.value = '';
+    discribe.value = '';
+    document.body.removeChild(document.getElementById('message'));
+}
+
+let name = document.getElementById('f-name');
+let email = document.getElementById('f-email');
+let subject = document.getElementById('f-subject');
+let discribe = document.getElementById('f-describe');
+
+
+/* slider */
+
+let items = document.querySelectorAll('.slider .item');
+let currentItem = 0;
+let isEnabled = true;
+
+function changeCurrentItem(n) {
+	currentItem = (n + items.length) % items.length;
+}
+
+function hideItem(direction) {
+	isEnabled = false;
+	items[currentItem].classList.add(direction);
+	items[currentItem].addEventListener('animationend', function() {
+		this.classList.remove('active', direction);
+	});
+}
+
+function showItem(direction) {
+	items[currentItem].classList.add('next', direction);
+	items[currentItem].addEventListener('animationend', function() {
+		this.classList.remove('next', direction);
+		this.classList.add('active');
+		isEnabled = true;
+	});
+}
+
+function nextItem(n) {
+	hideItem('to-left');
+	changeCurrentItem(n + 1);
+	showItem('from-right');
+}
+
+function previousItem(n) {
+	hideItem('to-right');
+	changeCurrentItem(n - 1);
+	showItem('from-left');
+}
+
+document.querySelector('.slider__left').addEventListener('click', function() {
+	if (isEnabled) {
+		previousItem(currentItem);
+	}
+});
+
+document.querySelector('.slider__right').addEventListener('click', function() {
+	if (isEnabled) {
+		nextItem(currentItem);
+	}
+});
+
+
+/*iphone disabled */
+let verticalPhoneDisabled = document.querySelector('.iphone1');
+ let horizontalPhoneDisabled = document.querySelector('.iphone2');
+ let verticalPhone = document.querySelector('.iphone-one');
+ let horizontalPhone = document.querySelector('.iphone-two');
+
+ verticalPhone.addEventListener('click', function() {
+     disablePhone(verticalPhoneDisabled);
+ });
+ verticalPhoneDisabled.addEventListener('click', function() {
+     disablePhone(verticalPhoneDisabled);
+ })
+ horizontalPhone.addEventListener('click', function() {
+     disablePhone(horizontalPhoneDisabled);
+ });
+
+ horizontalPhoneDisabled.addEventListener('click', function() {
+     disablePhone(horizontalPhoneDisabled);
+ });
+
+ function disablePhone(phoneType) {
+     if (phoneType.classList.contains('disabled')) {
+         phoneType.classList.remove('disabled');
+     } else {
+         phoneType.classList.add('disabled');
+     }
+ }
